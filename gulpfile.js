a271d2aaa7b688f9b0a66fa2c6122272b7f0e8a0
juancjara <juanc.jara@pucp.pe>
2014-12-07 16:12:05 -0500

    minify files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+minifyCSS = require('gulp-minify-css');
 //var react = require('gulp-react');
 
 var stylusDir = './assets/styles/src/*.styl';
@@ -10,7 +13,7 @@ var coffeeDir = './assets/js/src/*.coffee';
 
 gulp.task('css', function () {
   gulp.src(stylusDir)
-      .pipe(stylus({compress: false}))
+      .pipe(stylus({compress: true}))
       .pipe(gulp.dest('./assets/styles'));
 });
 /*
@@ -32,4 +35,31 @@ gulp.task('watch', function() {
   gulp.watch(coffeeDir, ['coffee'])
 });
 
+gulp.task('compressJS', function() {
+  gulp.src(
+      [
+        './assets/js/api.js',
+        './assets/js/vendor/react.min.js',
+        './assets/js/key.js',
+        './assets/js/dispatcher.js',
+        './assets/js/build/*.js',
+        './assets/js/main.js',
+      ]
+    )
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('./assets/js/min'))
+});
+
+gulp.task('minify-css', function() {
+  gulp.src([
+      './assets/styles/*.css',
+    ])
+    .pipe(minifyCSS())
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./assets/styles/'))
+});
+
 gulp.task('default', ['watch', 'coffee']);
+
+gulp.task('pro', ['compressJS', 'minify-css'])
