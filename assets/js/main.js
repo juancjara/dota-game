123b793547d4cf3a38cb949184ad6f7277486126
juancjara <juanc.jara@pucp.pe>
2014-12-10 22:07:37 -0500

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,23 @@ itemMng = new ItemManager();
 
 listChallenge = [
   [
-    {name:'emp', srcImg: 'emp', src: 'extraSkills'}, 
-    {name:'tornado', srcImg: 'tornado', src: 'extraSkills'},
-    {name:'sun strike', srcImg: 'sun_strike', src: 'extraSkills'}, 
+    {name:'eul', srcImg: 'eul', src: 'items'}, 
+    {name:'sun strike', srcImg: 'sun_strike', src: 'extraSkills'},
     {name:'chaos meteor', srcImg: 'chaos_meteor', src: 'extraSkills'},
     {name:'deafening blast', srcImg: 'deafening_blast', src: 'extraSkills'}
-  ],
-  [
+  ] , [
     {name:'forge spirit', srcImg: 'forge_spirit', src: 'extraSkills'}, 
     {name:'cold snap', srcImg: 'cold_snap', src: 'extraSkills'},
     {name:'emp', srcImg: 'emp', src: 'extraSkills'}, 
     {name:'ice wall', srcImg: 'ice_wall', src: 'extraSkills'},
     {name:'alacrity', srcImg: 'alacrity', src: 'extraSkills'}
-  ]
+  ], [
+    {name:'emp', srcImg: 'emp', src: 'extraSkills'}, 
+    {name:'tornado', srcImg: 'tornado', src: 'extraSkills'},
+    {name:'sun strike', srcImg: 'sun_strike', src: 'extraSkills'}, 
+    {name:'chaos meteor', srcImg: 'chaos_meteor', src: 'extraSkills'},
+    {name:'deafening blast', srcImg: 'deafening_blast', src: 'extraSkills'}
+  ] 
 ]
 
 $(function() {
